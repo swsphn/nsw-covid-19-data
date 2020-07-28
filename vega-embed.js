816a04cb72349bb6374a1d85f56ea87f9cde58ab
David Wales <david.wales@swsphn.com.au>
2020-07-28 13:44:07 +1000

    Add vega-lite code

---
+++ vega-embed.js
@@ -0,0 +1,49 @@
+// Assign the specification to a local variable.
+var daily_and_cumulative_cases_spec = {
+  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  autosize: 'fit',
+  width: "container",
+  height: "container",
+  //data: {"url": "covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv"},
+  data: {"url": "https://davidwales.github.io/nsw-covid-19-data/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv"},
+  transform: [{
+    "window": [{"op": "count", "field": "notification_date", "as": "cumulative_count"}],
+    "frame": [null,0]
+  }],
+  layer: [
+    {
+      mark: 'bar',
+      encoding: {
+        x: {timeUnit: "yearmonthdate", field: "notification_date"},
+        y: {
+          aggregate: 'count',
+          field: 'lga_name19',
+          type: 'quantitative',
+          axis: {
+            title: 'Daily Cases'
+          }
+        },
+        color: {field: "lhd_2010_name"}
+      },
+    },
+    {
+      mark: 'line',
+      encoding: {
+        x: {timeUnit: "yearmonthdate", field: "notification_date", title: "Date"},
+        y: {
+          aggregate: 'max',
+          field: "cumulative_count",
+          type: "quantitative",
+          axis: {
+            title: 'Cumulative Daily Cases'
+          }
+        }
+      }
+    }
+  ],
+  resolve: {scale: {y: "independent"}}
+};
+
+// Embed the visualization in the container with id `vis`
+//vegaEmbed('#vis', vlSpec);
+vegaEmbed('#daily_and_cumulative_cases', daily_and_cumulative_cases_spec);
