6cb8316733f40468558eb6899b5c25d1aef434dd
David Wales <david.wales@swsphn.com.au>
2020-07-28 16:27:59 +1000

    Add daily cases by likely infection source

--- vega-embed.js
+++ vega-embed.js
@@ -4,8 +4,7 @@ var daily_and_cumulative_cases_spec = {
   autosize: 'fit',
   width: 'container',
   height: 'container',
-  //data: {'url': 'covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv'},
-  data: {'url': 'https://davidwales.github.io/nsw-covid-19-data/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv'},
+  data: {url: 'https://davidwales.github.io/nsw-covid-19-data/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv'},
   transform: [
     {
       filter: {field: 'lhd_2010_name', equal: 'South Western Sydney'}
@@ -49,6 +48,39 @@ var daily_and_cumulative_cases_spec = {
   resolve: {scale: {y: 'independent'}}
 };
 
+var daily_cases_by_transmission_and_daily_tests_spec = {
+  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  autosize: 'fit',
+  width: 'container',
+  height: 'container',
+  layer: [
+    {
+      data: {url: 'https://davidwales.github.io/nsw-covid-19-data/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv'},
+      transform: [
+        {
+          filter: {field: 'lhd_2010_name', equal: 'South Western Sydney'}
+        }
+      ],
+      mark: 'bar',
+      encoding: {
+        x: {timeUnit: 'yearmonthdate', field: 'notification_date', type: 'temporal'},
+        y: {
+          aggregate: 'count',
+          field: 'likely_source_of_infection',
+          type: 'quantitative',
+          axis: {
+            title: 'Daily Cases'
+          }
+        },
+        color: {field: 'likely_source_of_infection', type: 'nominal'}
+      }
+    }//,
+    //{
+      //data: {url: 'https://davidwales.github.io/nsw-covid-19-data/covid-19-tests-by-date-and-postcode-local-health-district-and-local-government-area-aggregated.csv'},
+    //}
+  ]
+};
+
 // Embed the visualization in the container with id `vis`
-//vegaEmbed('#vis', vlSpec);
 vegaEmbed('#daily_and_cumulative_cases', daily_and_cumulative_cases_spec);
+vegaEmbed('#daily_cases_by_transmission_and_daily_tests', daily_cases_by_transmission_and_daily_tests_spec);
