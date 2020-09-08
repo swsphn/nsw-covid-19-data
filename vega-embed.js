660f47641dbcb4d12aa436a839bca8a9b3108e48
David Wales <david.wales@swsphn.com.au>
2020-09-08 15:13:08 +1000

    Add LGA postcode charts

--- vega-embed.js
+++ vega-embed.js
@@ -210,6 +210,69 @@ var cumulative_cases_by_SWS_LGA_spec = {
   }
 }
 
+var cumulative_cases_by_postcode_template = {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  width: "container",
+  height: "container",
+  config: sws_phn_config,
+  title: null,
+  selection: {date: {type: 'interval', bind: 'scales', encodings: ['x']}},
+  resolve: {scale: {y: 'independent'}},
+  data: {
+    url: "https://davidwales.github.io/nsw-covid-19-data/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv"
+  },
+  transform: [
+    {
+      filter: {
+        and: [
+          {field: "lhd_2010_name", equal: "South Western Sydney"},
+          {field: "lga_name19", equal: null}
+        ]
+      }
+    },
+    {
+      aggregate: [{op: "count", as: "date_postcode_count"}],
+      groupby: ["notification_date", "postcode"]
+    },
+    {
+      window: [
+        {op: "sum", field: "date_postcode_count", as: "cum_date_postcode_count"}
+      ],
+      groupby: ["postcode"],
+      frame: [null, 0]
+    }
+  ],
+  mark: {type: "line", tooltip: true},
+  encoding: {
+    x: {
+      timeUnit: "yearmonthdate",
+      field: "notification_date",
+      title: "Date",
+      type: "temporal"
+    },
+    y: {field: "cum_date_postcode_count", title: "Cumulative Cases", type: "quantitative"},
+    color: {
+      field: "postcode",
+      type: "nominal",
+      title: "Postcode",
+      legend: {orient: "top", columns: 5}
+    }
+  }
+}
+
+let LGAs = ["Camden", "Campbelltown", "Canterbury-Bankstown", "Fairfield", "Liverpool", "Wingecarribee", "Wollondilly"]
+
+LGAs.forEach(function (LGA) {
+    // Apparently the only way to easily deep copy an object in plan JS is to round trip through JSON strings!!!
+    // This is not the best solution in general, but should work for Vega Lite specs, as they are JSON specs.
+    let vega_lite_spec = JSON.parse(JSON.stringify(cumulative_cases_by_postcode_template))
+    console.log(vega_lite_spec)
+    vega_lite_spec.title = "Cumulative COVID-19 Cases by postcode in " + LGA
+    vega_lite_spec.transform[0].filter.and[1].equal = LGA
+    vegaEmbed('#cumulative_cases_by_postcode_' + LGA + '_LGA', vega_lite_spec);
+  }
+)
+
 // Embed the visualization in the container with id `vis`
 vegaEmbed('#daily_and_cumulative_cases', daily_and_cumulative_cases_spec);
 vegaEmbed('#daily_cases_by_transmission_and_daily_tests', daily_cases_by_transmission_and_daily_tests_spec);
